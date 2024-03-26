220d2505243e25c3619960e0bfe74b308df334ae
larsonscott89 <scottlarson0305@gmail.com>
2024-03-26 16:02:48 -0400

    serializers fixed

--- back-end/tickit/serializers.py
+++ back-end/tickit/serializers.py
@@ -2,35 +2,46 @@ from rest_framework import serializers
 from .models import Location, Venue, Event
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
-    venue = serializers.HyperlinkedRelatedField(
-        view_name='venue_detail',
-        many=False,
-        read_only=True
+    venue = serializers.PrimaryKeyRelatedField(
+        queryset = Venue.objects.all(),
+        source = 'venue_id'
+    )
+
+    event_url = serializers.ModelSerializer.serializer_url_field(
+      view_name = 'event_detail',
     )
 
     class Meta:
         model = Event
-        fields = ('venue', 'name', 'event_type', 'date', 'seating_type', 'cost', 'event_img')
+        fields = ('id', 'venue', 'name', 'event_type', 'date', 'seating_type', 'cost', 'event_img', 'event_url')
 
 class VenueSerializer(serializers.HyperlinkedModelSerializer):
-    events = serializers.HyperlinkedRelatedField(
-        view_name='event-detail',
-        many=True,
-        read_only=True
+    event = EventSerializer(
+        many = True,
+        read_only = True
+    )
+
+    city = serializers.ReadOnlyField(source='location.city')
+    state = serializers.ReadOnlyField(source='location.state')
+
+    location_url = serializers.HyperlinkedRelatedField(
+       view_name = 'location_detail',
+       source = 'locations',
+       read_only = True
     )
 
     class Meta:
         model = Venue
-        fields = ('location', 'name', 'venue_img', 'events')
+        fields = ('id', 'city', 'state', 'location_url', 'name', 'venue_img', 'event')
 
 class LocationSerializer(serializers.ModelSerializer):
   venues = VenueSerializer(
-    many=True,
-    read_only=True
+    many = True,
+    read_only = True
   )
   
   class Meta:
     model = Location
-    fields = ('state', 'city', 'city_img', 'venues')
+    fields = ('id', 'state', 'city', 'city_img', 'venues')
 
 
