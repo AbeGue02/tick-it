ff84239bfffe1681c5669a316f0e32e01afe8eff
larsonscott89 <scottlarson0305@gmail.com>
2024-03-26 14:52:12 -0400

    backend and frontend filter

--- front-end/tick-it/src/screens/VenueList.jsx
+++ front-end/tick-it/src/screens/VenueList.jsx
@@ -1,8 +1,39 @@
+import {useEffect, useState} from 'react'
+
+const url = 'http://localhost:8000'
+
 export default function VenueList() {
-    //Lists all the venues from the database and their basic info (Name and location)
+
+    const [data, setData] = useState([])
+    const [searchText, setSearchText] = useState('')
+
+    useEffect(() => {
+        fetchData()
+    },[searchText])
+
+    const fetchData = async() => {
+        const endpoint = `${url}/venues?venues=${searchText}`
+
+        try {
+            const response = await fetch(endpoint, {
+                method: 'GET'
+            })
+            const data = await response.json()
+            console.log(data)
+            setData(data)
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+
     return (
-        <div>
-            VenueList
+        <div className='app'>
+            <input type='search' placeholder='search' value={searchText} onChange={e=> setSearchText(e.target.value)}/>
+            <ul>
+                {data && data.map(el => <li key={el.id}>{el.name}</li>)}
+            </ul>
+            
         </div>
     )
 }
