8844fc81699cf3a94df22d59625d64ba16dbfb19
larsonscott89 <scottlarson0305@gmail.com>
2024-03-27 15:02:07 -0400

    venuelist redirect to events

--- front-end/tick-it/src/components/VenueListItem.jsx
+++ front-end/tick-it/src/components/VenueListItem.jsx
@@ -1,11 +1,18 @@
-export default function VenueListItem (props) {
-    
+import { useNavigate } from 'react-router-dom'
+
+export default function VenueListItem(props) {
+    const navigate = useNavigate()
+
+    const handleVenueClick = () => {
+        navigate(`/venues/${props.venue.id}/events`)
+    }
+
     return (
-        <div className='venueListItemContainer'>
-                <h3>{props.venue.name}</h3>
-                <h4>{props.venue.location}</h4>
+        <div className='eventListItemContainer' onClick={handleVenueClick} 
+            style={{backgroundImage: `linear-gradient(rgba(0,0,0,0.0), rgba(0,0,0,0.7)), url(${props.venue.venue_img})`}}
+        >
+            <h3>{props.venue.name}</h3>
+            <h4>{props.venue.city}, {props.venue.state}</h4>
         </div>
-       
-       
     )
 }
--- front-end/tick-it/src/screens/VenueList.jsx
+++ front-end/tick-it/src/screens/VenueList.jsx
@@ -1,12 +1,45 @@
-
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import VenueListItem from '../components/VenueListItem'
 
 export default function VenueList() {
+    const [venues, setVenues] = useState([])
+    const { venueId } = useParams()
+    const [events, setEvents] = useState([])
+    const [searchBarText, setSearchBarText] = useState('')
 
-    
+    const fetchVenues = async () => {
+        let response = await axios.get('http://localhost:8000/venues')
+        setVenues(response.data)
+    }
+
+    useEffect(() => {
+        fetchVenues()
+    }, [])
+
+    // const getEvents = async () => {
+    //     let response;
+    //     if (venueId) {
+    //         response = await axios.get(`http://localhost:8000/venues/${venueId}`)
+    //     } else {
+    //         response = await axios.get('http://localhost:8000/events')
+    //     }
+    //     venueId 
+    //     ? setEvents(searchBarText ? response.data.event.filter((event) => event.name.includes(searchBarText)) : response.data.event)
+    //     : setEvents(searchBarText ? response.data.filter((event) => event.name.includes(searchBarText)) : response.data)
+    //     console.log(response.data)
+    // }
+
+    // useEffect(() => {
+    //     getEvents();
+    // }, [venueId]); // Fetch events whenever venueId changes
 
     return (
         <div>
-            Venue List
+            {venues.map((venue) => (
+                <VenueListItem key={venue.id} venue={venue} />
+            ))}
         </div>
     )
 }
