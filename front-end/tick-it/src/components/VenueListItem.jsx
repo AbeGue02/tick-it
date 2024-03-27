1c8b5eddb6f586b9cc090284509495319e7d28f0
Zekkune <christian.adams22405@gmail.com>
2024-03-27 10:53:30 -0400

    test

--- front-end/tick-it/src/components/VenueListItem.jsx
+++ front-end/tick-it/src/components/VenueListItem.jsx
@@ -1,7 +1,3 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { useNavigate, useParams } from 'react-router-dom'
-
 export default function VenueListItem (props) {
     
     return (
