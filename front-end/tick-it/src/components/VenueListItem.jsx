31c0f61350f7e4b1894f2a853c61ea580aa40f5f
Zekkune <christian.adams22405@gmail.com>
2024-03-27 10:28:21 -0400

    removing mapping from VenueListItem component added venue location

--- front-end/tick-it/src/components/VenueListItem.jsx
+++ front-end/tick-it/src/components/VenueListItem.jsx
@@ -3,30 +3,30 @@ import axios from 'axios'
 import { useNavigate, useParams } from 'react-router-dom'
 
 export default function VenueListItem (props) {
-    const [location, setLocation] = useState([])
+    // const [location, setLocation] = useState([])
 
-    useEffect(() => {
-        try {
-            const response = axios.get(`localhost:8000/locations`)
-            console.log(response)
-            setLocation(response)
-            console.log(location)
-        } catch (error) {
-            console.error('Error fetching meal:', error)
-        }
+    // const getLocation = async () => {
+    //     let response = await axios.get('http://127.0.0.1:8000/locations/')
+    //     setLocation(response.data)
+    //     console.log(response.data)
+    // }
+
+    // useEffect(() => {
+    //     try {
+    //         getLocation
+    //     } catch (error) {
+    //         console.error('Error fetching meal:', error)
+    //     }
         
-    }, [])
+    // }, [])
 
     return (
-        <div>
-        <ul>
-            {props.venues.map(venue => (
-                <div className='venueListItemContainer'>
-                <h3>{venue.name}</h3>
-                <h4>{venue.location}</h4>
-                </div>
-            ))}
-        </ul>
+        <div className='venueListItemContainer'>
+                <h3>{props.venue.name}</h3>
+                <h4>{props.venue.location}</h4>
+                <h4>{location}</h4>
         </div>
+       
+       
     )
 }
