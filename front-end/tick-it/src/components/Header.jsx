f3b58125a97c2d421188bc4e9507935c47d44d35
larsonscott89 <scottlarson0305@gmail.com>
2024-03-26 22:53:28 -0400

    home venues events links added and work

--- front-end/tick-it/src/components/Header.jsx
+++ front-end/tick-it/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     // Contains buttons to venues and event screens
@@ -7,10 +8,12 @@ export default function Header() {
 
     return (
         <header className="headerContainer">
+            <Link to='/' className='home-button'>
             <h1>Tick-IT</h1>
+            </Link>
             <nav>
-                <button onClick={() => navigate('/venues')}>Venues</button>
-                <button>Events</button>
+            <Link to='/venues' className='venue-button'><button onClick={() => navigate('/venues')}>Venues</button></Link>
+            <Link to='/events' className='event-button'><button>Events</button></Link>
             </nav>
         </header>
     )
