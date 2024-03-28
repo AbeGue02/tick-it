ec0df7329f1c387052caef44e5e623a3386f2ac4
AbeGue02 <abrahamgue02@gmail.com>
2024-03-28 09:37:30 -0400

    Makes styles better and adds deleteEvent button

--- back-end/tickit_django/settings.py
+++ back-end/tickit_django/settings.py
@@ -83,9 +83,7 @@ WSGI_APPLICATION = 'tickit_django.wsgi.application'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': []
 }
 
 # Database
--- front-end/tick-it/src/screens/EventDetails.jsx
+++ front-end/tick-it/src/screens/EventDetails.jsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import SearchBar from '../components/SearchBar'
-import EventListItem from '../components/EventListItem'
+import { useNavigate } from 'react-router-dom'
 
 export default function EventDetails() {
 
-    const { eventId, venueId } = useParams()
+    const { eventId } = useParams()
     const [events, setEvents] = useState([])
-    const [venues, setVenues] = useState([])
+    let navigate = useNavigate()
 
     const getEventDetail = async () => {
         let response = eventId ? 
@@ -18,19 +17,29 @@ export default function EventDetails() {
         console.log(response.data)
     }
 
+    const deleteEvent = async () => {
+        let response = await axios.delete(`http://localhost:8000/events/${eventId}`)
+        console.log(response)
+        navigate('/events')
+    }
+
     useEffect(() => {
         getEventDetail()
     }, [])
 
-    
+
     return (
         <div className='eventsDetailsContainer'>
             <img className='event-image' src={events.event_img} alt="" />
-            <h2>{events.name} @ {new Date(events.date).toLocaleString()}</h2>
-            <h3>{events.venue_name}</h3>
-            <h3>{events.event_city}, {events.event_state}</h3>
+            <h3>{events.name} @ {new Date(events.date).toLocaleString()}</h3>
+            <h4>{events.venue_name}</h4>
+            <h4>{events.event_city}, {events.event_state}</h4>
+            <iframe style={{border: 0, width: '100%'}} loading="lazy" allowFullScreen src={`https://www.google.com/maps/embed/v1/search?q=${`${events.venue_name}%2C%20${events.event_city}%2C%20${events.event_state}`}&key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`}></iframe>
             <h3>{events.seating_type}</h3>
             <h3>${events.cost}</h3>
+            <button 
+                className='deleteButton'
+                onClick={deleteEvent}>Delete Event</button>
         </div>
     )
 }
